583bb6f6fc66d7201c8fb2c23a4b31290bccd75a
johnkiguma <154498415+johnkiguma@users.noreply.github.com>
2024-01-09 10:02:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+let cats =['Milo', 'Otis', 'Garfield']
+function beforeEach(cats){
+  return length = 0;
+}
+
+function destructivelyAppendCat(name){
+  cats.push('Ralph')
+  return cats
+}
+function destructivelyPrependCat(name){
+  cats.unshift('Bob')
+  return cats
+}
+function destructivelyRemoveLastCat(){
+  cats.pop('Garfield')
+  return cats
+}
+function destructivelyRemoveFirstCat(){
+  cats.shift('Milo')
+  return cats
+}
+function appendCat(){
+  return cats.concat('Broom')
+}
+function prependCat() {
+  return['Arnold']. concat(cats)
+}
+function removeLastCat(){
+  return cats.slice(0, -1)
+}
+function removeFirstCat(){
+  return cats.slice(1)
+}
